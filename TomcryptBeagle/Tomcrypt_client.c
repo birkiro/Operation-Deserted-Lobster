1c2b25791ce174795c6502c2051e44418cd369c9
b4me <holbbs@gmail.com>
2013-11-20 21:38:01 +0100

    encrypt and decrypt functions in place and working, MAX plain text size is 50 bytes!

--- TomcryptBeagle/Tomcrypt_client.c
+++ TomcryptBeagle/Tomcrypt_client.c
@@ -1,12 +1,18 @@
 #include <tomcrypt.h>
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once
+#define MAXDATASIZE 	100 	// max number of bytes we can get at once
+#define PLAINTEXT_SIZE	50		// lenght of pt
+#define CIPHER_SIZE		1024	// lenght of cipher
 char buf[MAXDATASIZE];
 
 int err, hash_idx, prng_idx, res;
 rsa_key private_key, public_key;
 FILE* file;
 char private_key_file[200], public_key_file[200];
+unsigned char pt_in[PLAINTEXT_SIZE], pt_out[PLAINTEXT_SIZE]; //for plain text both ways
+unsigned long pt_lenght = PLAINTEXT_SIZE;
+unsigned char cipher_in[CIPHER_SIZE], cipher_out[CIPHER_SIZE]; //for encrypted msg's both ways
+unsigned long cipher_lenght = CIPHER_SIZE;
 
 int key_generator(void) {
 	/* make an RSA-1024 key */
@@ -70,56 +76,119 @@ int key_import() {
 	 *	Where Private are used to decrypt received msg, and public to encrypt msg to server
 	 */
 
-
 	file = fopen("public_key", "r");
 
-	fseek(file, 0, SEEK_END); 	// seek to end of file
-	int key_size = ftell(file);	// get current file pointer
-	fseek(file, 0, SEEK_SET); 	// seek back to beginning of file
-								// proceed with allocating memory and reading the file
+	fseek(file, 0, SEEK_END); // seek to end of file
+	int key_size = ftell(file); // get current file pointer
+	fseek(file, 0, SEEK_SET); // seek back to beginning of file
+							  // proceed with allocating memory and reading the file
 
 	char import_key_array[key_size];
 
 	unsigned long i = 0;
 	for (i = 0; i < key_size; ++i) {
 		fscanf(file, "%c", &import_key_array[i]);
-		printf("%c",import_key_array[i]);
+		printf("%c", import_key_array[i]);
 	}
 	fclose(file);
 	printf("\n\n");
 
 	file = fopen("private_key", "r");
 
-	fseek(file, 0, SEEK_END); 	// seek to end of file
-	key_size = ftell(file);	// get current file pointer
-	fseek(file, 0, SEEK_SET); 	// seek back to beginning of file
-								// proceed with allocating memory and reading the file
+	fseek(file, 0, SEEK_END); // seek to end of file
+	key_size = ftell(file); // get current file pointer
+	fseek(file, 0, SEEK_SET); // seek back to beginning of file
+							  // proceed with allocating memory and reading the file
 
 	unsigned char import_key_array2[key_size];
 
-
 	for (i = 0; i < key_size; ++i) {
 		fscanf(file, "%c", &import_key_array2[i]);
-		printf("%c",import_key_array2[i]);
+		printf("%c", import_key_array2[i]);
 	}
 	fclose(file);
 	printf("\n\n");
 
 	unsigned long inlen = sizeof(import_key_array2);
 
-	if ((err = rsa_import(import_key_array2, inlen, &private_key)) != CRYPT_OK) {
-			printf("Export error: %s\n", error_to_string(err));
-			return -1;
-		}
+	if ((err = rsa_import(import_key_array2, inlen, &private_key))
+			!= CRYPT_OK) {
+		printf("Export error: %s\n", error_to_string(err));
+		return -1;
+	}
 	inlen = sizeof(import_key_array);
 
 	if ((err = rsa_import(import_key_array, inlen, &public_key)) != CRYPT_OK) {
-			printf("Export error: %s\n", error_to_string(err));
-			return -1;
-		}
-
+		printf("Export error: %s\n", error_to_string(err));
+		return -1;
+	}
 
 	printf("Keys imported.\n\n");
+	return 0;
+}
+
+int encrypt_msg() {
+
+	//cipher_lenght = sizeof(cipher_out);
+
+	if ((err = rsa_encrypt_key(pt_out, /* data we wish to encrypt */
+
+			pt_lenght, /* data is 16 bytes long */
+
+			cipher_out, /* where to store ciphertext */
+
+			&cipher_lenght, /* length of ciphertext */
+
+			"TestApp", /* our lparam for this program */
+
+			7, /* lparam is 7 bytes long */
+
+			NULL, /* PRNG state */
+
+			prng_idx, /* prng idx */
+
+			hash_idx, /* hash idx */
+
+			&public_key) /* our RSA key */
+
+	) != CRYPT_OK) {
+
+		printf("rsa_encrypt_key %s", error_to_string(err));
+
+		return EXIT_FAILURE;
+
+	}
+	return 0;
+}
+
+int decrypt_msg() {
+	//pt_lenght = sizeof(pt2);
+
+	if ((err = rsa_decrypt_key(cipher_in, /* encrypted data */
+
+			cipher_lenght, /* length of ciphertext */
+
+			pt_in, /* where to put plaintext */
+
+			&pt_lenght, /* plaintext length */
+
+			"TestApp", /* lparam for this program */
+
+			7, /* lparam is 7 bytes long */
+
+			hash_idx, /* hash idx */
+
+			&res, /* validity of data */
+
+			&private_key) /* our RSA key */
+
+	) != CRYPT_OK) {
+
+		printf("rsa_decrypt_key %s", error_to_string(err));
+
+		return EXIT_FAILURE;
+
+	}
 	return 0;
 }
 
@@ -160,6 +229,15 @@ int main(int argc, char *argv[]) {
 			break;
 		case '3':
 			printf("3: Initiate communication\n");
+			strcpy((char*) pt_out, "test input string here!!! wuuhuu");
+			encrypt_msg();
+			unsigned long i = 0;
+			for (; i < CIPHER_SIZE; i++) {
+				cipher_in[i] = cipher_out[i];
+			}
+			decrypt_msg();
+			printf("pt_in = %s\n", pt_in);
+			printf("pt_out = %s\n", pt_out);
 
 			break;
 		default:
